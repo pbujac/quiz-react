29fd2d22fe9106627841b7953eeedb8a5857fab2
Andrei Timchiv <atimchiv@pentalog.fr>
2017-08-23 14:13:50 +0300

    deleted manifest.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Login from './Login'
 
 const App = () =>(
   <div> 
       It's gonna contain the header , footer and content.
-      <Login/>
   </div>
 )
 
--- src/components/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Login = () =>(
-  <div> 
-      There are gon be the log inputs .
-  </div>
-)
-
-export default Login ;

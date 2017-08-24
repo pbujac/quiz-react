246ceb088a84d49642d9f156c7e6e5664f67cefd
pbujac <pbujac@pentalog.fr>
2017-08-24 11:43:18 +0300

    added sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import './index.css'
 
-const root = document.querySelector('#root')
-ReactDOM.render(<App />, root)
+const root = document.querySelector('#root');
+ReactDOM.render(<App />, root);

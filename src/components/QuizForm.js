f29dbf6da04dbdf2d8d7e5fc78cf80a515c7a810
pbujac <pbujac@pentalog.fr>
2017-08-28 15:48:11 +0300

    created quiz form

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -1,16 +1,56 @@
 import React from 'react';
+import {Paper, RaisedButton, TextField} from "material-ui";
+import PropTypes from 'prop-types';
 
 const QuizForm = ({
                       onSubmit,
                       onChange,
                       errors,
                       successMessage,
-                      user
+                      quiz
                   }) => {
 
     return (
-        <div></div>
+        <section className="quiz-detail">
+
+            <Paper zDepth={1} className='card'>
+
+                <form action="/" onSubmit={onSubmit}>
+                    <h2 className="card-heading">Login</h2>
+
+                    {successMessage && <p className="success-message">{successMessage}</p>}
+                    {errors.summary && <p className="error-message">{errors.summary}</p>}
+
+                    <TextField
+                        floatingLabelText="Title"
+                        name="title"
+                        errorText={errors.title}
+                        onChange={onChange}
+                        value={quiz.title}
+                    />
+
+                    <TextField
+                        floatingLabelText="Description"
+                        name="description"
+                        onChange={onChange}
+                        errorText={errors.description}
+                        value={quiz.description}
+                    />
+
+                    <RaisedButton type="submit" label="Create Quiz" primary/>
+                </form>
+
+            </Paper>
+        </section>
     );
+};
+
+QuizForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired,
+    successMessage: PropTypes.string.isRequired,
+    quiz: PropTypes.object.isRequired
 };
 
 export default QuizForm;
--- src/containers/QuizCreate.js
+++ src/containers/QuizCreate.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import QuizForm from "../components/QuizForm";
 
 class QuizCreate extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.processForm = this.processForm.bind(this);
+    }
+
     render() {
         return (
-            <div></div>
+            <QuizForm
+                onSubmit={this.processForm}
+                onChange={this.changeUser}
+                errors={this.state.errors}
+                successMessage={this.state.successMessage}
+                quiz={this.state.quiz}
+            />
         );
     }
 }

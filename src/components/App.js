532eb63118b378c7afea30c403cac3de017c04f0
vvacarov <vvacarov@pentalog.fr>
2017-08-28 11:07:20 +0300

    add Router in index, delete from App, delete routes.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import {MuiThemeProvider} from "material-ui/styles/index";
-import BrowserRouter from "react-router-dom/es/BrowserRouter";
+import {Route} from "react-router-dom";
 import Header from "./Common/Header";
 import Footer from "./Common/Footer";
-import Routes from './routes.js';
+import Home from "./Home";
 import './App.css';
 
 const App = () => (
     <div className="App">
-        <MuiThemeProvider>
-            <BrowserRouter>
-                <div>
-                    <Header/>
-                    <Routes/>
-                    <Footer/>
-                </div>
-            </BrowserRouter>
-        </MuiThemeProvider>
+        <Header/>
+        <Route exact path='/' component={Home}/>
+        <Footer/>
     </div>
 );
 
